c1085150237357da43e8d6c116564ea325f933c4
Eamon Boonzaaier <eamon@symanticreative.com>
2025-03-16 00:37:47 +0200

    updated

--- example-client.js
+++
@@ -1,9 +0,0 @@
-const { VendureAdminClientFactory } = require('./dist');
-
-// Create a client instance
-const client = VendureAdminClientFactory.createClient({
-  apiUrl: 'https://api.shopeazy.co/admin-api',
-});
-
-console.log('Created VendureAdminClient instance successfully');
-console.log(client);
--- fix-permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x build.sh
-chmod +x link-to-admin.sh
-echo "Scripts are now executable"
